542d5ecda1e27ea6c9e5420d90767a0f8241d852
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-06-28 11:50:38 -0500

    test next 6

---
+++ LevelUpCenter.Test/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace LevelUpCenter.Test.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ LevelUpCenter.Test/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace LevelUpCenter.Test.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
